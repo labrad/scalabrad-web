b900ab172684e592dad1cce7431194b5175ef653
Adam Whiteside <adamcwhiteside@gmail.com>
2016-06-26 20:44:09 -0700

    Changed master dependency to tag r78

--- client-js/config.js
+++ client-js/config.js
@@ -13,7 +13,7 @@ System.config({
     "d3": "github:mbostock/d3@3.5.17",
     "three": "github:mrdoob/three.js@master",
     "page": "npm:page@1.6.3",
-    "three": "github:mrdoob/three.js@master",
+    "three": "github:mrdoob/three.js@r78",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
     "github:jspm/nodelibs-process@0.1.1": {
