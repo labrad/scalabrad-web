34db1c46b61ce73f1e8785b99956c0841e2215a1
Adam Whiteside <adamcw@users.noreply.github.com>
2016-06-26 21:50:44 -0700

    Updated d3 to v3.5.17 (#203)
    Updated to the latest d3 since it's preferred to be on the latest version if it doesn't break anything. v4.0.0 is going to bring a lot of changes, so keeping up to date should avoid dependence on older features.

--- client-js/config.js
+++ client-js/config.js
@@ -10,7 +10,7 @@ System.config({
 
 System.config({
   "map": {
-    "d3": "github:mbostock/d3@3.5.6",
+    "d3": "github:mbostock/d3@3.5.17",
     "page": "npm:page@1.6.3",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
