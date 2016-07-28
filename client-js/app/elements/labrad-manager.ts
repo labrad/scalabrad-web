e38e8b1342d5ac4bc638ee9e05584fe3e305a500
Adam Whiteside <adamcwhiteside@gmail.com>
2016-07-28 11:26:07 -0700

    Added a confirmation dialog to closing connections.

--- client-js/app/elements/labrad-manager.ts
+++ client-js/app/elements/labrad-manager.ts
@@ -6,9 +6,36 @@ export class LabradManager extends polymer.Base {
   @property({type: Array, notify: true})
   connections: Array<ConnectionInfo>;
 
+  @property({type: Object, notify: true})
+  selectedConnection: {connId: number, name: string};
+
   mgr: ManagerApi;
 
   closeConnection(event) {
-    this.mgr.connectionClose(event.currentTarget.connId);
+    this.selectedConnection = {
+      connId: event.currentTarget.connId,
+      name: event.currentTarget.connName
+    };
+
+    const dialog = this.$.closeConnectionConfirmation;
+    dialog.open();
+
+    event.preventDefault();
+  }
+
+  closeConnectionConfirmed() {
+    const id = this.selectedConnection.connId;
+
+    this.mgr.connectionClose(id);
+    this.selectedConnection = null;
+
+    // Remove the connection from the array to notify the UI
+    const numConnections = this.connections.length;
+    for (let i = 0; i < numConnections; ++i) {
+      if (this.connections[i].id == id) {
+        this.splice('connections', i, 1);
+        break;
+      }
+    }
   }
 }
