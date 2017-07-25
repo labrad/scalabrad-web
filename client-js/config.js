efc798e0397cdaf952f6941cb7821e9ecaa09fdd
Matthew Neeley <maffoo@google.com>
2017-07-25 10:08:01 -0700

    wip - Use local version of polymer-ts instead of bower package

--- client-js/config.js
+++ client-js/config.js
@@ -12,6 +12,7 @@ System.config({
   "map": {
     "d3": "github:mbostock/d3@3.5.17",
     "page": "npm:page@1.6.3",
+    "polymer-ts": "npm:polymer-ts@0.1.26",
     "three": "github:mrdoob/three.js@r78",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
