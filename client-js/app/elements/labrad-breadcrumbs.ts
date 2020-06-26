258d737bb8c518f2dd69d4338a2667f620fc3f56
Ganden Schaffner <gschaffner@pm.me>
2020-06-25 17:51:35 -0700

    Translate labrad-breadcrumb element to use polymer-decorators

--- client-js/app/elements/labrad-breadcrumbs.ts
+++ client-js/app/elements/labrad-breadcrumbs.ts
@@ -1,11 +1,68 @@
+import {PolymerElement, html} from "@polymer/polymer";
+import {customElement, property} from "@polymer/decorators";
+
 export interface BreadcrumbItem {
   name: string;
   isLink: boolean;
   url?: string;
 }
 
-@component('labrad-breadcrumbs')
-export class LabradBreadcrumbs extends polymer.Base {
+@customElement('labrad-breadcrumbs')
+export class LabradBreadcrumbs extends PolymerElement {
+
+  static get template(): HTMLTemplateElement {
+    return html`
+      <style>
+        ul {
+          padding-left: 0.5em;
+          display: inline;
+          white-space: nowrap;
+          list-style-type: none;
+        }
+        li {
+          display: inline;
+        }
+        ul.path {
+          padding: 0;
+        }
+        li.dir:before {
+          content: "/";
+        }
+        li.extra {
+          margin-left: 0.5em;
+        }
+        a {
+          color: white;
+        }
+      </style>
+      <ul class="path">
+        <template is="dom-repeat" items="{{breadcrumbs}}">
+          <li class="dir">
+            <template is="dom-if" if="{{item.isLink}}">
+              <a is="app-link" path="{{item.url}}" href="{{item.url}}"><span>{{item.name}}</span></a>
+            </template>
+            <template is="dom-if" if="{{!item.isLink}}">
+              <span>{{item.name}}</span>
+            </template>
+          </li>
+        </template>
+        <template is="dom-if" if="{{extras}}">
+          <li class="extra">|</li>
+        </template>
+        <template is="dom-repeat" items="{{extras}}">
+          <li class="extra">
+            <template is="dom-if" if="{{item.isLink}}">
+              <a is="app-link" path="{{item.url}}" href="{{item.url}}"><span>{{item.name}}</span></a>
+            </template>
+            <template is="dom-if" if="{{!item.isLink}}">
+              <span>{{item.name}}</span>
+            </template>
+          </li>
+        </template>
+      </ul>
+    `;
+  }
+
   @property({type: Array, notify: true})
   breadcrumbs: BreadcrumbItem[];
 
